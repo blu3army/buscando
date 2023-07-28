649936c4629a987ad85af194f915c97234578033
blu3army <edadfeudal@gmail.com>
2023-07-27 22:44:19 -0300

    Terminado widget de topics

--- build/front-end/components/home/topics-comp.js
+++ build/front-end/components/home/topics-comp.js
@@ -1,11 +1,11 @@
 import { html } from "../../preact.js";
 import { categoryTree } from "../../databases/categorias-db.js";
-export function TopicsComp({ cityCode }) {
-    return html `<div class="flex flex-col sm:flex-row flex-wrap ">
+export function TopicsComp({ cityCode, fn }) {
+    return html `<div id="inner-div" class="flex absolute top-2" style="left: 0px;">
         ${categoryTree.map((cat) => {
         if (cat.level === 0) {
             return html `<a href="/home/${cityCode}?topic=${cat.code}">
-                    <div class="m-1 py-1 px-2 border border-blue-200 rounded-xl" style="">
+                    <div class="m-1 py-1 px-2 border border-blue-200 rounded-xl flex flex-col items-center w-40 h-18" style="">
                         <span class="mr-2 text-blue-500">
                             <i class=${cat.icon}></i>
                         </span>
